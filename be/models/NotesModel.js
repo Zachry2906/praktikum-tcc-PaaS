080d8e840c05df49f0ecaa595e605e0502510d2a
Ahmad Zakaria <116710753+Zachry2906@users.noreply.github.com>
2025-04-11 13:32:30 +0700

    Update NotesModel.js

--- be/models/NotesModel.js
+++ be/models/NotesModel.js
@@ -3,7 +3,7 @@ import db from "../config/database.js"
 
 const { DataTypes } = Sequelize;
 
-const Notes = db.define("notes", {
+const Notes = db.define("notessyifa", {
   Title: DataTypes.STRING,
   Content: DataTypes.STRING
 });
